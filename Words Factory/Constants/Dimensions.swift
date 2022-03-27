561b0db9b68aad79ff978244a11d9493cb4802a9
Edward <tamerlan701@gmail.com>
2022-03-28 05:46:53 +0700

    Dimensions and Strings constants -Also added comments to font constants

--- Words Factory/Constants/Dimensions.swift
+++ Words Factory/Constants/Dimensions.swift
@@ -1,5 +1,7 @@
-import Foundation
+import UIKit
 
 enum Dimensions {
-    
+    static let smallMargin: CGFloat = 8
+    static let standartMargin: CGFloat = 16
+    static let mediumMargin: CGFloat = 32
 }
