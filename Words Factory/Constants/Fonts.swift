c3123495c81df1e559d03fd591a4a7e7ab588977
Edward <tamerlan701@gmail.com>
2022-03-28 05:00:02 +0700

    Added fonts and constants for it

--- Words Factory/Constants/Fonts.swift
+++ Words Factory/Constants/Fonts.swift
@@ -13,3 +13,19 @@ extension UIFont {
             ?? .systemFont(ofSize: size)
     }
 }
+
+extension UIFont {
+    static let heading1 = rubik(.bold, size: 56)
+    static let heading2 = rubik(.bold, size: 40)
+    static let heading3 = rubik(.bold, size: 32)
+    static let heading4 = rubik(.medium, size: 24)
+    static let heading5 = rubik(.medium, size: 20)
+    
+    static let paragraphLarge = rubik(.regular, size: 16)
+    static let paragraphMedium = rubik(.regular, size: 14)
+    static let paragraphSmall = rubik(.medium, size: 12)
+    
+    static let buttonLarge = rubik(.medium, size: 18)
+    static let buttonMedium = rubik(.medium, size: 16)
+    static let buttonSmall = rubik(.medium, size: 14)
+}
