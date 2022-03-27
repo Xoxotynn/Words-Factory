731964ccdb95004a4c9907a159123c3ea9876cab
Edward <tamerlan701@gmail.com>
2022-03-28 04:44:30 +0700

    Color, image, app icon assets -Added constants for colors and images

--- Words Factory/Constants/Images.swift
+++ Words Factory/Constants/Images.swift
@@ -1,5 +1,22 @@
 import UIKit
 
 extension UIImage {
+    // MARK: Images
+    static let hiTechKid = UIImage(named: "HiTechKid")
+    static let relationshipKids = UIImage(named: "LongDistanceRelationshipKids")
+    static let skateboardKid = UIImage(named: "SkateboardKid")
+    static let standingKid = UIImage(named: "StandingKid")
+    static let standingKid2 = UIImage(named: "StandingKid2")
+    static let stayingHomeKid = UIImage(named: "StayingHomeKid")
     
+    // MARK: Icons
+    static let hideIcon = UIImage(named: "HideIcon")
+    static let showIcon = UIImage(named: "ShowIcon")
+    static let searchIcon = UIImage(named: "SearchIcon")
+    static let soundIcon = UIImage(named: "SoundIcon")
+    
+    // MARK: Tab bar icons
+    static let dictionaryIcon = UIImage(named: "DictionaryIcon")
+    static let trainingIcon = UIImage(named: "TrainingIcon")
+    static let videoIcon = UIImage(named: "VideoIcon")
 }
